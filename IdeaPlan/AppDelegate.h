48ddcd24af94e6dc44289e45238f521278e6a35b
chargechen <1431941564@qq.com>
2014-12-21 21:29:34 +0800

    Initial Commit

---
+++ IdeaPlan/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IdeaPlan
+//
+//  Created by chargechen on 14/12/21.
+//  Copyright (c) 2014年 JUNBAO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IdeaPlan/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IdeaPlan
+//
+//  Created by chargechen on 14/12/21.
+//  Copyright (c) 2014年 JUNBAO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
